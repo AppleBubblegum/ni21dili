b2d9c68b17f14fe64aed02cb99eea1218333bccf
isama <isa.baghirov@ufaz.az>
2021-11-10 17:04:34 +0100

    My First commit

---
+++ ni21dili/function.py
@@ -0,0 +1,9 @@
+import numpy as np
+from ipywidgets import interact, fixed
+from PIL import Image
+
+
+def imshow(X, resize=None):
+	img = Image.fromarray(X)
+	img.resize(size=(resize[0], resize[1]))
+	return img
